5db41d037e498b7306fb38adca41662eadb059ef
Nizar Rahman <102470420+nizarrahman@users.noreply.github.com>
2022-09-29 17:28:42 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -6,22 +6,16 @@ let str = `*https://youtube.com/channel/UC6GPl9xMWL61NAXQb3HBrRw*
 
 Pengen mentahan nya ?
 Nih ada santai bro....
-https://github.com/ImYanXiao/Elaina-MultiDevice
-
-Mau Yang No Error Script Bot nya, Gas Cek Video YT :
-https://youtube.com/channel/UC6GPl9xMWL61NAXQb3HBrRw
-
-Pengen yang udah di recode oleh owner?
-Script bot ini dijual bila kalian mau bisa chat owner
-
-wa.me/6285380166282`
+Ada Syarat Nya Follow Dulu Ig gw
+https://instagram.com/nizarrrahman.f_
+wa.me/6281386945391`
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
     mediaUrl: "https://Instagram.com/maxxy_ofc",
     mediaType: "VIDEO",
     description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: '𝑴𝒂𝒂𝒙𝒚 𝑩𝒐𝒕𝒛 MultiDevice',
+    title: '𝙉𝘼𝙄-𝘿𝙚𝙫𝙚𝙡𝙤𝙥𝙚𝙧 MultiDevice',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
