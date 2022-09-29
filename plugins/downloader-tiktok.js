da567ed272cbdfcc033d94e12f62be9f3646d92b
Nizar Rahman <102470420+nizarrahman@users.noreply.github.com>
2022-09-29 17:30:27 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -21,7 +21,7 @@ if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/
 *Nickname:* ${nickname}
 *Description:* ${description}
 
-_©𝑴𝒂𝒂𝒙𝒚 𝑩𝒐𝒕𝒛_
+_©Nizar Rahman
 `.trim(), m)
 }
 handler.help = ['tiktok', 'tiktok', 'tiktokdl'].map(v => v + ' <url>')
