a9145402bec3ae37b4c7f6b44944dfb78d0b5662
Nizar Rahman <102470420+nizarrahman@users.noreply.github.com>
2022-09-29 17:29:20 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://youtube.com/channel/UC6GPl9xMWL61NAXQb3HBrRw*
+let str = `*https://instagram.com/nizarrrahman.f_*
 
 Pengen mentahan nya ?
 Nih ada santai bro....
@@ -12,7 +12,7 @@ wa.me/6281386945391`
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/maxxy_ofc",
+    mediaUrl: "https://Instagram.com/nizarrrahman.f_",
     mediaType: "VIDEO",
     description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
     title: '𝙉𝘼𝙄-𝘿𝙚𝙫𝙚𝙡𝙤𝙥𝙚𝙧 MultiDevice',
